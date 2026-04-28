343b619bbaacbf4553b711ac68cb9ccea8283b39
WilliamJoshammar <william.joshammar@skola.taby.se>
2026-04-28 10:53:20 +0200

    Update server.js

--- API/server.js
+++ API/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-
+ 
 // Middleware
 app.use(express.json());
-
+ 
 // Routes
 app.use("/", require("./routes/docs"));
 app.use("/books", require("./routes/books"));
-
+app.use("/login", require("./routes/auth"));
+ 
 // Starta servern
 app.listen(PORT, () => {
   console.log(`Servern körs på http://localhost:${PORT}`);
 });
+ 
